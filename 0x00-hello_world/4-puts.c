a880b0d1c49593ef0df6f67bfc17b15a1f045085
YHaouane <yassirhaouane8@gmail.com>
2023-03-16 17:49:25 -0700

    things are getting harder task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - this is the main function
+ * main - this is the main funtion
  *
  * Return: always 0
  */
-int main (void)
+
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}		
+}
