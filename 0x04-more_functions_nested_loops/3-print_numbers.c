56698744b8863fc6ed0c63745dd5772d13c7440a
YHaouane <yassirhaouane8@gmail.com>
2023-03-24 11:15:49 -0700

    numbers 0to9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,13 +3,15 @@
 /**
  * print_numbers - prints the numbers from 0 to 9
  *
- * Return: void
+ * Return: the numbers from 0 to 9
  */
 void print_numbers(void)
 {
     int i;
-    for (i = 0; i <= 9; i++) {
-        _putchar(i + '0');
+
+    for (i = 0; i <= 9; i++) 
+    {
+	_putchar(i + '0');
     }
     _putchar('\n');
 }
