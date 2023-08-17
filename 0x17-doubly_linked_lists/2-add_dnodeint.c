536cc4436b685d63827fa4fc543c97c5a5beef49
root <yassirhaouane8@gmail.com>
2023-08-17 11:00:08 -0700

    i wanna die instead of programming in C again

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - add a new node to the beginning of a doubly linked list
+ * @head: double pointer to the list
+ * @n: data to be inserted in the  new node
+ * Return: the address of the new element, or NULL if it failed
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	if (!head)
+		return (NULL);
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
+	new->n = n;
+	new->next = *head;
+	new->prev = NULL;
+	if (*head)
+		(*head)->prev = new;
+	*head = new;
+	return (new);
+}
---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,1 @@
+well those C tasks that i didnt even check before the forced defer arer now the problem of my life because i hate C and i don't know how i did even pass the month with full C programming language
